8d78b66a311bdf80540dc597995b89b6e32dbe82
William Okou <gokou097@gmail.com>
2023-05-25 16:46:57 +0100

    url update

--- src/utils/trpc-provider.tsx
+++ src/utils/trpc-provider.tsx
@@ -18,9 +18,8 @@ export const TrpcProvider: React.FC<{children: React.ReactNode}> = ({
   );
 
   const url = process.env.NEXT_PUBLIC_VERCEL_URL
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc/`
-    : 'http://localhost:3000/api/trpc/';
-  console.log(url);
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
+    : 'http://localhost:3000/api/trpc';
 
   const [trpcClient] = useState(() =>
     trpc.createClient({
