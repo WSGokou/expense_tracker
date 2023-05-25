fc5ce41d5a07a19c04e7bd841deb2effdc7f83f8
William Okou <gokou097@gmail.com>
2023-05-25 15:51:44 +0100

    logging url for checking on preview deployment

--- src/utils/trpc-provider.tsx
+++ src/utils/trpc-provider.tsx
@@ -20,6 +20,7 @@ export const TrpcProvider: React.FC<{children: React.ReactNode}> = ({
   const url = process.env.NEXT_PUBLIC_VERCEL_URL
     ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
     : 'http://localhost:3000/api/trpc/';
+  console.log(url);
 
   const [trpcClient] = useState(() =>
     trpc.createClient({
