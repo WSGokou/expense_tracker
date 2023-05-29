425b804311ebb23de682edeaa35c84e9f15d22be
William Okou <gokou097@gmail.com>
2023-05-29 13:55:37 +0100

    CORS changed

--- next.config.js
+++ next.config.js
@@ -4,14 +4,12 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: '/api/(.*)',
+        source: '/api/:path*',
         headers: [
           {key: 'Access-Control-Allow-Credentials', value: 'true'},
           {
             key: 'Access-Control-Allow-Origin',
-            value: process.env.NEXT_PUBLIC_VERCEL_URL
-              ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-              : 'http://localhost:3000',
+            value: '*',
           },
           {
             key: 'Access-Control-Allow-Methods',
